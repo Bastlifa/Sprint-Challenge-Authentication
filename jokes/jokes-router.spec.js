bf5f882411cd4ee5ca53076e086dd83c6eb008e3
Eli Sacks <bastlifa@gmail.com>
2019-09-20 15:18:21 -0400

    er, jokes tests now actually added

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -24,7 +24,40 @@ describe('jokes-router', () => {
                         .set('Accept', 'application/json')
                         .then(res =>
                             {
-                                expect(res.body.token).toBeTruthy()
+                                // expect(res.body.token).toBeTruthy()
+                                return request(server)
+                                .get('/api/jokes')
+                                .set('authorization', `${res.body.token}`)
+                                .then(res =>
+                                    {
+                                        expect(res.status).toBe(200)
+                                    })
+                            })
+                    })
+        })
+        it('gives a 401 when no token is provided', () =>
+        {
+            const user = {username: 'test2', password: 'pass2'}
+            return request(server)
+                .post('/api/auth/register')
+                .send(user)
+                .set('Accept', 'application/json')
+                .then(res =>
+                    {
+                        return request(server)
+                        .post('/api/auth/login')
+                        .send(user)
+                        .set('Accept', 'application/json')
+                        .then(res =>
+                            {
+                                // expect(res.body.token).toBeTruthy()
+                                return request(server)
+                                .get('/api/jokes')
+                                // .set('authorization', `${res.body.token}`)
+                                .then(res =>
+                                    {
+                                        expect(res.status).toBe(401)
+                                    })
                             })
                     })
         })
