0257278d0b4b7dea9dc939cdc5f66ba3aeb8bdc7
Eli Sacks <bastlifa@gmail.com>
2019-09-20 11:36:40 -0400

    auth router set up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,77 @@
 const router = require('express').Router();
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Users = require('./authHelper')
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+router.post('/', (req, res) =>
+{
+  if(!req.body.username || !req.body.password)
+  {
+    res.status(400).json({ errorMessage: "Missing username or password" })
+  }
+  else
+  {
+    let {username, password} = req.body
+    bcryptjs.genSalt(14, function(err, salt)
+    {
+      bcryptjs.hash(password, salt, function(err, hash)
+      {
+        password = hash
+        Users.add({username, password})
+          .then(response =>
+            {
+              res.status(201).json({id: response.id, username: response.username})
+            })
+          .catch(error =>
+            {
+              res.status(500).json({ errorMessage: `Could not register user` })
+            })
+      })
+    })
+  }
+})
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+router.post('/login', (req, res) =>
+{
+  if(!req.body.username || !req.body.password)
+  {
+    res.status(400).json({ errorMessage: "Missing credentials" })
+  }
+  else
+  {
+    let {username, password} = req.body
+    Users.findBy({ username })
+      .first()
+      .then(user =>
+        {
+          if(user)
+          {
+            bcryptjs.compare(req.body.password, user.password, function(err, response)
+            {
+              if(response)
+              {
+                const token = generateToken(user)
+                res.status(200).json({token})
+              }
+              else res.status(401).json({ message: 'Invalid Credentials' })
+            })
+          }
+          else res.status(401).json({ message: 'Invalid Credentials' })
+        })
+      .catch(error =>
+        {
+            res.status(500).json(error)
+        })
+  }
+})
+
+function generateToken(user)
+{
+  const payload = {username: user.username}
+  const secret = require('../config/secret').jwtSecret
+  const options = {expiresIn: '1d'}
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
